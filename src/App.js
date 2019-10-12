c223e914ac902e19cee5cd9dbc6aa39e8324d632
dmahon02 <38335422+dmahon02@users.noreply.github.com>
2019-10-12 01:20:34 -0400

    asdf

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Helmet } from 'react-helmet';
 // } from 'react-router-dom'
 
 import ButtonLabel from './components/ButtonLabel'
+import Grid from './components/Grid'
 
 const App = () => {
   return (
@@ -15,19 +16,9 @@ const App = () => {
       <Helmet>
         <title>SIS 2.0</title>
         <meta charSet="utf-8" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
-        />
-        <script
-          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-          crossorigin
-        />
       </Helmet>
       <body>
-        <ButtonLabel name="Johnny" />
+        <Grid name="Johnny" />
       </body>
     </div>
   );
