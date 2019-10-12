a944e8fa2d20e8735d0f71d39ccb4639cabfaf31
Camille <camille.calabrese@gmail.com>
2019-10-12 00:25:41 -0400

    starting drop down list file

---
+++ src/components/search page/distdropdown.js
@@ -0,0 +1,6 @@
+/* distdropdown.js
+ * A drop down menu for selecting which distribution requirements you need a
+ *      class to fill
+ * Camille Calabrese -- my first JavaScript file :-)
+ * Polyhack 2019
+ */
